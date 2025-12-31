94089faa28b5a16bcff2211d859dbcce63d50570
Ameer Hamza <hamzarajput7896@gmail.com>
2025-12-31 21:22:40 +0500

    Create app and models folder and write a dummy data for weather forcasting in weather_data file

---
+++ models/weather_data.py
@@ -0,0 +1,47 @@
+
+
+weather_data = {
+
+
+    "lahore": {
+
+        "current_weather" : "Sunny",
+        "wind_speed" : 3.5,
+        "after_1_hour" : "Partly Cloud"
+
+    },
+
+    "karcahi": {
+
+        "current_weather" : "Humidity",
+        "wind_speed" : 4.7,
+        "after_1_hour" : "Sunny"
+        
+    },
+
+    "new_york" : {
+
+        "current_weather" : "Rainy",
+        "wind_speed" : "6.3",
+        "after_1_hour" : "light rain"
+
+    },
+
+    "london" : {
+
+        "current_weather" : "snow",
+        "wind_speed" : 5.6,
+        "after_1_hour" : "cloudy"
+
+    },
+
+    "tokyo" : {
+
+        "current_weather" : "clear",
+        "wind_speed" : 1.9,
+        "after_1_hour" : "cloudy"
+
+    }
+
+    
+}
