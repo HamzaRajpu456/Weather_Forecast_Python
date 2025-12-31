6535a232d4c4f63a4b3c1d5d20c649b5a37562f4
Ameer Hamza <hamzarajput7896@gmail.com>
2025-12-31 21:59:35 +0500

    create a Class WeatherApp having __init_ and show weather functions in  show weather function do validation about city come from user and then show data about weather forcast that is comes from weather data

---
+++ app/weather.py
@@ -0,0 +1,28 @@
+from models.weather_data import Weather_Data
+
+
+class WeatherApp:
+
+    def __init__(self):
+
+        self.weather_data = Weather_Data
+
+
+    def show_weather(self, city):
+        city = city.lower()
+
+        if city not in self.weather_data:
+            print("Invalid City Name!")
+            return  
+
+
+        data = self.weather_data[city]
+
+        print("Weather Report")
+        print("City", city.title())
+        print("Current_weather", data["current_weather"])
+        print("Wind_Speed", data["wind_speed"])
+        print("Weather after 1 Hour", data["after_1_hour"])
+    
+
+
